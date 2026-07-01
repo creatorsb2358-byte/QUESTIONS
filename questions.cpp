fd6f0f460ec9256cf71d50e707dd52a1fdae7d46
shaurya Negi <creatorsb2358@gmail.com>
2026-07-01 15:30:50 +0530

    done with my 7 question

---
+++ questions.cpp
@@ -0,0 +1,18 @@
+// 7. question in pattern;
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n = 5;
+    for(int i = 1; i <= n; i++){
+        for(int j = 0; j < n-i; j++){
+            cout << " ";
+        }
+        for(int j = 0; j <2*i-1; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
