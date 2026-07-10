1982b9fb458b9b255e143e39301f8070712ab15a
shaurya Negi <creatorsb2358@gmail.com>
2026-07-10 20:53:02 +0530

    how to find smallest and largest number in array

--- questions.cpp
+++ questions.cpp
@@ -1,20 +1,33 @@
-// binary to decimal:
+/// to find the smallest number in an array;
 
 #include <iostream>
 using namespace std;
 
 int main(){
-    int binary ;
-    cout << "choose binary for conversion:";
-    cin >> binary;
-    int power = 1;
-    int decimal = 0;
-    while (binary > 0){
-        int remainder = binary % 10;
-        binary /= 10;
-        decimal += remainder*power ;
-        power *= 2;
-
+    int marks[5] = {1,2,3,4,5};
+    int small = INT32_MAX;
+    for(int i = 0; i < 5; i++){
+        if(marks[i] < small ){
+            small = marks[i];
+        }
+        
     }
-    cout << decimal ; 
+    cout << small << endl;
+    return 0 ;
+
+}
+
+
+/// to find the largest number in an array;
+
+int main1(){
+    int marks[5] = {1,2,3,4,5};
+    int largest = INT32_MIN;
+    for(int i = 0; i < 5; i++){
+        if(marks[i] > largest){
+            largest = marks[i];
+        }
+    }
+    cout << largest << endl;
+    return 0;
 }
