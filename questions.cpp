22c47deba75627b97c2e1c044d64b8d1089f73a1
shaurya Negi <creatorsb2358@gmail.com>
2026-07-12 23:53:06 +0530

    to take intersection of 2 arrays

--- questions.cpp
+++ questions.cpp
@@ -1,20 +1,20 @@
-///to print the unique element in an array:
+///to take intersection of 2 arrays:
 
 #include <iostream>
 using namespace std;
 
 int main(){
-    int arr[8] ={1,2,3,1,2,3,4,5};
-    for(int i = 0; i < 8; i++){
+    int arr1[4] = {1,2,3,4};
+    int arr2[5] = {4,5,6,7,8};
+    for(int i = 0 ; i < 4; i++){
         int count = 0;
-        for(int j = 0; j< 8; j++){
-            if(arr[i] == arr[j]){
+        for(int j = 0; j < 5; j++){
+            if(arr1[i] == arr2[j]){
                 count++;
             }
         }
-        if(count == 1){
-            cout << arr[i] << " ";
+        if(count > 0){
+            cout << arr1[i] << " " ;
         }
     }
 }
-
