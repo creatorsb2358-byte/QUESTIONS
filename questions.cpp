be1aa932008876847bef2b3b2c8758d04269eb3e
shaurya Negi <creatorsb2358@gmail.com>
2026-07-13 22:28:06 +0530

    reversing a vector

--- questions.cpp
+++ questions.cpp
@@ -1,20 +1,25 @@
-///to take intersection of 2 arrays:
+///reversing a vector: 
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int main(){
-    int arr1[4] = {1,2,3,4};
-    int arr2[5] = {4,5,6,7,8};
-    for(int i = 0 ; i < 4; i++){
-        int count = 0;
-        for(int j = 0; j < 5; j++){
-            if(arr1[i] == arr2[j]){
-                count++;
-            }
-        }
-        if(count > 0){
-            cout << arr1[i] << " " ;
-        }
+int swapping_arr(vector<int>& nums, int size){
+    int start = 0; 
+    int end = size - 1;
+    while(start <= end ){
+        swap(nums[start],nums[end]);
+        start++;
+        end--;
     }
+}
+
+int main(){
+    vector<int> num = {1,2,3,4,5};
+    
+    swapping_arr(num , 5);
+    for(int i : num ){
+        cout << i;
+    }
+    
 }
