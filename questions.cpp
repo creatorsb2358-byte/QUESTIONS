a1a9faba15604fbd4731854306e8a69fb65a548d
shaurya Negi <creatorsb2358@gmail.com>
2026-07-14 23:02:23 +0530

    pairsum

--- questions.cpp
+++ questions.cpp
@@ -1,19 +1,29 @@
-///kadane's algorithm:
+///pairsum:
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int main(){
-    int arr[7] = {3,-4,5,4,-1,7,-8};
-    int maxx = INT16_MIN;
-    int cs = 0;
-    for(int i = 0; i < 7; i++){
-        cs += arr[i];
-        maxx = max(cs,maxx);
-        if(cs < 0){
-            cs = 0;
+vector<int> pairsum(vector<int>& nums, int target){
+    int i = 0 , j = 3;
+    while(i <  j ){
+        int pairsum = nums[i] + nums[j];
+        if(pairsum > target){
+            j--;
+        }else if (pairsum < target){
+            i++;
+        }else {
+            nums.push_back(i);
+            nums.push_back(j);
+            return nums;
         }
+    }
 }
-cout << maxx;
-}        
+
+int main(){
+    vector<int> nums  = {2,7,11,15};
+    int target = 9;
+    vector<int> ans = pairsum(nums , target );
+    cout << ans[0] << " " << ans[1] ;
+}
 
