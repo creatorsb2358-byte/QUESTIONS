a38d4b932c8405d794ec25953bf7a4741bab373a
shaurya Negi <creatorsb2358@gmail.com>
2026-07-16 22:49:26 +0530

    Intersection of two arrays

--- questions.cpp
+++ questions.cpp
@@ -1,23 +1,33 @@
-///remove duplicates in a sorted array:
+///Intersection of two arrays:
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main(){
-    int nums[5] = {1,2,3,3,4};
-    int size = 5;
-    int st = 0 , end = 1;
-    while(end < size){
-        if(nums[st] == nums[end]){
-            end++;
-        }else{
-            st++;
-
-            nums[st] = nums[end];
-            end++;
-              
+    vector<int> nums1 = {1,2,3,4};
+    vector<int> nums2 = {2,3};
+    int size1 = nums1.size();
+    int size2 = nums2.size();
+    vector<int> ans ;
+        
+    for(int i = 0; i<size1 ; i++){
+        for(int j = 0; j< size2 ; j++){
+            if(nums1[i] == nums2[j]){
+                bool found = false ;
+                for(int val : ans){ 
+                    if(nums1[i] == val ){
+                        found = true;
+                    }
+                }if(found == false ){
+                    ans.push_back(nums1[i]);
+                }
+            }
         }
+            
     }
-    cout << st + 1;
+    for(int i : ans){
+        cout << i;
     }
 
+}
