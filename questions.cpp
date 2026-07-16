cbac64e33b64067c29d5ee5fe10617bab7cef2f2
shaurya Negi <creatorsb2358@gmail.com>
2026-07-16 22:52:04 +0530

    move zeroes

--- questions.cpp
+++ questions.cpp
@@ -1,33 +1,23 @@
-///Intersection of two arrays:
+///move zeroes:
 
 #include <iostream>
 #include <vector>
 using namespace std;
 
 int main(){
-    vector<int> nums1 = {1,2,3,4};
-    vector<int> nums2 = {2,3};
-    int size1 = nums1.size();
-    int size2 = nums2.size();
-    vector<int> ans ;
-        
-    for(int i = 0; i<size1 ; i++){
-        for(int j = 0; j< size2 ; j++){
-            if(nums1[i] == nums2[j]){
-                bool found = false ;
-                for(int val : ans){ 
-                    if(nums1[i] == val ){
-                        found = true;
-                    }
-                }if(found == false ){
-                    ans.push_back(nums1[i]);
-                }
+    vector<int> nums = {0,1,2,0,4,5};
+    int size = nums.size();
+        int st = 0, end = 0;
+        for(int i = 0 ; i<size;i++){
+            if(nums[end] != 0){
+                swap(nums[st],nums[end]);
+                st++;
+                end++;
+            }else if(nums[end] == 0){
+                end++;
             }
         }
-            
-    }
-    for(int i : ans){
-        cout << i;
-    }
-
+        for(int i : nums){
+            cout << i;
+        }
 }
