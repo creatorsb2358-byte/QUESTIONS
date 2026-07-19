f7882649e1c9d4bc88389f89fca470875eea5f36
shaurya Negi <creatorsb2358@gmail.com>
2026-07-20 00:17:01 +0530

    container with most water:

--- questions.cpp
+++ questions.cpp
@@ -1,24 +1,24 @@
-///pow(x,n):
+///container with most water:
 
 #include <iostream>
 #include <vector>
 using namespace std;
 
 int main(){
-    double x = 3 , n = 5;
-    ///ans start from here :
-    int binary_form = n ;
-    if(n < 0){
-        x = 1/x;
-        binary_form = -binary_form;
-    } 
-    double ans = 1 ;
-    while(binary_form > 0){
-        if(binary_form % 2 == 1){
-            ans *= x ;
-        }
-        x *= x;
-        binary_form /= 2;
+    vector<int> height = {1,8,6,2,5,4,8,3,7};
+    int ans = 0;
+    int size = height.size();
+    int st = 0, end = size-1;
+    while(st <= end){
+        int breath = end - st;
+        int heigh = min(height[st],height[end]);
+        int area = breath * heigh ;
+        ans = max(ans , area); 
+        height[st] < height[end] ? st++:end--;
+
     }
-    cout << ans;
+    cout << "the maximum area is :" << ans;
+
+
 }
+
