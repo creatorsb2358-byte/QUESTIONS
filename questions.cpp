930bde9bf023c8a0b3bd73519abbbfd3be39d356
shaurya Negi <creatorsb2358@gmail.com>
2026-07-20 13:21:26 +0530

    product of array except itself

--- questions.cpp
+++ questions.cpp
@@ -1,24 +1,27 @@
-///container with most water:
+///product of array except itself:
 
 #include <iostream>
 #include <vector>
 using namespace std;
 
 int main(){
-    vector<int> height = {1,8,6,2,5,4,8,3,7};
-    int ans = 0;
-    int size = height.size();
-    int st = 0, end = size-1;
-    while(st <= end){
-        int breath = end - st;
-        int heigh = min(height[st],height[end]);
-        int area = breath * heigh ;
-        ans = max(ans , area); 
-        height[st] < height[end] ? st++:end--;
-
+    vector<int> nums = {1,2,3,4};
+    int size = nums.size();
+    vector<int> preffix(size, 1) ;
+    vector<int> suffix(size,1) ;
+    vector<int> ans(size,1);
+    
+    for(int i = 1 ; i < size ;i++ ){
+        preffix[i] = preffix[i-1] * nums[i-1];
     }
-    cout << "the maximum area is :" << ans;
-
-
+    for(int i = size -2 ; i>= 0; i--){
+        suffix[i] = suffix[i+1]*nums[i+1];
+    }
+    for(int i = 0 ;i< size ;i++){
+        ans[i] = preffix[i]*suffix[i];
+    }
+    for(int i : ans){
+        cout << i << endl ;
+    }
 }
 
