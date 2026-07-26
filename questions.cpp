c873b222c9bcaf299959faf42abc1716e656b3c2
shaurya Negi <creatorsb2358@gmail.com>
2026-07-27 00:14:07 +0530

    binary search(recursive method)

--- questions.cpp
+++ questions.cpp
@@ -1,21 +1,25 @@
-/// binary search 
+/// binary search (recursive method) 
 
 #include <iostream>
 using namespace std;
+
+int BS(int arr[] , int target , int st , int end){
+    if(st <= end){
+        int mid = st + (end-st)/2 ;
+        if(arr[mid] > target){
+            BS(arr , target , st , mid-1);
+        }else if(arr[mid] < target){
+            BS(arr , target , mid +1 , end);
+        }else {
+            cout << mid;
+        }
+    }
+    return -1;
+}
 
 int main(){
     int arr[] = {1,3,5,6,9,12};
     int target = 12;
     int st = 0, end = 5;
-    while(st <= end){
-        int mid = st + (end-st)/2;
-        if(arr[mid] < target){
-            st = mid+1;
-        }else if(arr[mid] > target){
-            end = mid-1;
-        }else{
-            return mid;
-        }
-    }
-    return -1;
+    BS(arr , target , st , end);
 }
