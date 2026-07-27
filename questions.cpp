3d906c2e2f6f11c96fe4c7df7fbcebad979aba6a
shaurya Negi <creatorsb2358@gmail.com>
2026-07-27 23:53:07 +0530

    peak index in a mountain array

--- questions.cpp
+++ questions.cpp
@@ -5,29 +5,19 @@
 using namespace std;
 
 int main(){
-    vector<int> nums = {5,6,7,8,0,1,2,3,4};
-    int target = 7;
-    int size = nums.size();
-    int st = 0 , end = size -1 ;
+    vector<int> arr = {5,6,7,8,4,3,2,1};
+    int size = arr.size();
+    int st = 1 , end = size-2;
     while(st <= end){
-        int mid = st + (end-st)/2;
-        if(nums[mid] == target){
+        int mid = st + (end - st)/2;
+        if(arr[mid-1] < arr[mid] && arr[mid] > arr[mid+1]){
             cout << mid;
         }
-        if(nums[st] <= nums[mid]){
-            if(nums[st] <= target  && target <= nums[mid]){
-                end = mid-1;
-            }else{
-                st = mid+1;
-            }
-        }
-        if(nums[mid] < nums[end]){
-            if(nums[mid] <= target  && target <= nums[end]){
-                st = mid+1;
-            }else{
-                end = mid-1;
-            }
+        if(arr[mid] < arr[mid+1]){
+            st = mid+1;
+        }else if(arr[mid] > arr[mid+1]){
+            end = mid-1;
         }
     }
-    return -1;
+    return 0;
 }
