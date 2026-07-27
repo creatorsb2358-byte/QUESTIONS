315684173b2c8d5143c725f6bec43e81fdc4af5c
shaurya Negi <creatorsb2358@gmail.com>
2026-07-27 23:46:30 +0530

    search in a rotated sorted array

--- questions.cpp
+++ questions.cpp
@@ -1,25 +1,33 @@
-/// binary search (recursive method) 
+///search in a rotated sorted array;
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int BS(int arr[] , int target , int st , int end){
-    if(st <= end){
-        int mid = st + (end-st)/2 ;
-        if(arr[mid] > target){
-            BS(arr , target , st , mid-1);
-        }else if(arr[mid] < target){
-            BS(arr , target , mid +1 , end);
-        }else {
+int main(){
+    vector<int> nums = {5,6,7,8,0,1,2,3,4};
+    int target = 7;
+    int size = nums.size();
+    int st = 0 , end = size -1 ;
+    while(st <= end){
+        int mid = st + (end-st)/2;
+        if(nums[mid] == target){
             cout << mid;
+        }
+        if(nums[st] <= nums[mid]){
+            if(nums[st] <= target  && target <= nums[mid]){
+                end = mid-1;
+            }else{
+                st = mid+1;
+            }
+        }
+        if(nums[mid] < nums[end]){
+            if(nums[mid] <= target  && target <= nums[end]){
+                st = mid+1;
+            }else{
+                end = mid-1;
+            }
         }
     }
     return -1;
-}
-
-int main(){
-    int arr[] = {1,3,5,6,9,12};
-    int target = 12;
-    int st = 0, end = 5;
-    BS(arr , target , st , end);
 }
