228ad953562c7ea3799a2b16e6b42cf81ae2571a
shaurya Negi <creatorsb2358@gmail.com>
2026-07-28 09:40:50 +0530

    single number in O(log n)

--- questions.cpp
+++ questions.cpp
@@ -1,23 +1,41 @@
-///search in a rotated sorted array;
+///single number in O(log n);
 
 #include <iostream>
 #include <vector>
 using namespace std;
 
 int main(){
-    vector<int> arr = {5,6,7,8,4,3,2,1};
-    int size = arr.size();
-    int st = 1 , end = size-2;
-    while(st <= end){
-        int mid = st + (end - st)/2;
-        if(arr[mid-1] < arr[mid] && arr[mid] > arr[mid+1]){
-            cout << mid;
+    vector<int> nums = {1,1,2,3,3,4,4,8,8};
+        int size = nums.size();
+        int st = 0 , end = size -1;
+        if(size == 1){
+            return nums[0];
         }
-        if(arr[mid] < arr[mid+1]){
-            st = mid+1;
-        }else if(arr[mid] > arr[mid+1]){
-            end = mid-1;
+        while(st <= end){
+            int mid = st + (end - st)/2;
+            if(mid == 0 && nums[mid] != nums[mid+1]){
+                return nums[mid];
+            }else if( mid == size-1 && nums[size-2] == nums[mid]){
+                return nums[mid];
+            }
+            if(nums[mid-1] != nums[mid] && nums[mid+1] != nums[mid]){
+                return nums[mid];
+            }
+            if(mid%2 == 0){
+                if(nums[mid - 1] == nums[mid]){
+                    end = mid-1;
+                }else if(nums[mid +1] == nums[mid]){
+                    st = mid+1;
+                }
+            }
+            if(mid % 2 != 0){
+                if(nums[mid+1] == nums[mid]){
+                    end = mid-1;
+                }else if(nums[mid -1] == nums[mid]){
+                    st = mid+1;
+                }
+            }
         }
-    }
-    return 0;
+        return 0;
+
 }
