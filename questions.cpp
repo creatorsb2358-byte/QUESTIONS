d8f9294efdc13962ec2336ccb190afabd5f90e03
shaurya Negi <creatorsb2358@gmail.com>
2026-07-29 18:47:23 +0530

    Subtract the Product and Sum of Digits of an Integer

--- questions.cpp
+++ questions.cpp
@@ -1,41 +1,22 @@
-///single number in O(log n);
+///Subtract the Product and Sum of Digits of an Integer;
 
 #include <iostream>
 #include <vector>
 using namespace std;
 
 int main(){
-    vector<int> nums = {1,1,2,3,3,4,4,8,8};
-        int size = nums.size();
-        int st = 0 , end = size -1;
-        if(size == 1){
-            return nums[0];
-        }
-        while(st <= end){
-            int mid = st + (end - st)/2;
-            if(mid == 0 && nums[mid] != nums[mid+1]){
-                return nums[mid];
-            }else if( mid == size-1 && nums[size-2] == nums[mid]){
-                return nums[mid];
-            }
-            if(nums[mid-1] != nums[mid] && nums[mid+1] != nums[mid]){
-                return nums[mid];
-            }
-            if(mid%2 == 0){
-                if(nums[mid - 1] == nums[mid]){
-                    end = mid-1;
-                }else if(nums[mid +1] == nums[mid]){
-                    st = mid+1;
-                }
-            }
-            if(mid % 2 != 0){
-                if(nums[mid+1] == nums[mid]){
-                    end = mid-1;
-                }else if(nums[mid -1] == nums[mid]){
-                    st = mid+1;
-                }
-            }
-        }
-        return 0;
+    int n = 234;
+    long long product = 1;
+    int sum = 0;
+    int ans = 0;
+    while(n > 0){
+        int remainder = n%10;
+        n /= 10;
+        product *= remainder;
+        sum += remainder ;
+           
+    }
+    ans = product - sum;
+    return ans;
 
 }
