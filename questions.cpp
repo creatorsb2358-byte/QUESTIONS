48e4891068d2c2a23e5ee437860e06447d4b5747
shaurya Negi <creatorsb2358@gmail.com>
2026-07-30 20:52:31 +0530

    book allocation problem

--- questions.cpp
+++ questions.cpp
@@ -1,22 +1,52 @@
-///Subtract the Product and Sum of Digits of an Integer;
+///Book allocation problem;
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <numeric>
 using namespace std;
 
-int main(){
-    int n = 234;
-    long long product = 1;
-    int sum = 0;
-    int ans = 0;
-    while(n > 0){
-        int remainder = n%10;
-        n /= 10;
-        product *= remainder;
-        sum += remainder ;
-           
-    }
-    ans = product - sum;
-    return ans;
+    bool canSplit(vector<int>&nums, int limitSum, int k){
 
+        int n = nums.size();
+        int currSum = 0, splitCnt = 1;
+        for(int i = 0 ; i < n ; i++){
+
+            if(currSum + nums[i] <= limitSum){
+                currSum += nums[i];
+            }
+
+            else{
+                currSum = nums[i];
+                splitCnt++;
+            }
+        }
+        return splitCnt <= k;
+    }
+    int splitArray(vector<int>& nums, int k) {
+
+        if(k > nums.size()) return -1;
+
+        int low = *max_element(nums.begin(), nums.end());
+        int high = accumulate(nums.begin(), nums.end(), 0);
+        while(low < high){
+         
+            int mid = low + (high - low) / 2;
+           
+            if(canSplit(nums, mid, k)){
+               high = mid;
+            }
+
+            else{
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+int main(){
+    vector<int> nums = {2,1,3,4};
+    int k = 2;
+    int result = splitArray(nums,k);
+    cout << result ;
 }
