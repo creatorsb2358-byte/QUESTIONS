601a660919c7535e3bff7257ede699739ec6fd96
shaurya Negi <creatorsb2358@gmail.com>
2026-07-31 23:53:36 +0530

    painter partition problem

--- questions.cpp
+++ questions.cpp
@@ -1,26 +1,50 @@
-///container with most water;
+//painter partition problem:
 
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <numeric>
 using namespace std;
 
+bool isvalid(vector<int>& arr , int m , int limit){
+    int size = arr.size();
+    int student = 1 , pages = 0;
+    for(int i = 0 ;i < size ; i++){
+        if(arr[i] > limit){
+            return false;
+        }
+        if(arr[i] + pages <= limit){
+            pages += arr[i];
+        }else{
+            student++;
+            pages = arr[i];
+        }
+    }
+    if(student > m){
+        return false;
+    }else{
+        return true;
+    }
+}
 
 int main(){
-    vector<int> height = {1,8,6,2,5,4,8,3,7};
-    int ans = 0;
-        
-    int size = height.size();
-    int st = 0, end = size-1;
-    while(st <= end){
-        int breath = end - st;
-        int heigh = min(height[st],height[end]);
-        int area = breath * heigh ;
-        ans = max(ans , area); 
-        height[st] < height[end] ? st++:end--;
-
+    vector<int> nums = {30,50,10,60} ;
+    int size = nums.size();
+    int sum = 0;
+    int minval = INT32_MIN;
+    for(int i = 0 ;i < size ; i++){
+        sum += nums[i];
+        minval = max(nums[i] , minval);
     }
-    return ans;
-        
+    int st = minval  , end = sum;
+    int ans = 0;
+    while(st <= end){
+        int mid = st + (end-st)/2;
+        if(isvalid(nums , 2 , mid)){
+            end = mid-1;
+            ans = mid;
+        }else{
+            st = mid+1;
+        }
+    }
+    cout << ans;
+
 }
