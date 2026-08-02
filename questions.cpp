50b82b91b3d61954d37ea76f8844a686fccfcdbe
shaurya Negi <creatorsb2358@gmail.com>
2026-08-02 23:17:38 +0530

    search inert position

--- questions.cpp
+++ questions.cpp
@@ -1,43 +1,25 @@
-///Koko eating banana Problem:
+///search inert position:
 
 #include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
-bool isvalid(vector<int>& nums, int h , int k){
+int main(){
+    vector<int> nums = {-1,3,5,6,11};
     int size = nums.size();
-    int hours = 0;
-    for(int pile : nums){
-        hours += pile / k;
-        if(pile % k != 0){
-            hours++;
-        }
-        if(hours > h){
-            return false;
-        }
-    }
-    return true;
-}
-
-int working(vector<int>& arr , int h){
-    sort(arr.begin(),arr.end());
-    int size = arr.size();
-    int st = 1 , end = arr[size-1], ans = -1;
+    int target = 9;
+    int st = 0 , end = size - 1;
     while(st <= end){
         int mid = st + (end-st)/2;
-        if(isvalid(arr , h , mid)){
-            end = mid-1;
-            ans = mid;
-        }else{
+        if(target == nums[mid]){
+            return mid;
+        }
+        if(target > nums[mid]){
             st = mid+1;
+        }else if(target < nums[mid]){
+            end = mid-1;
         }
     }
-    return ans;
-}
-
-int main(){
-    vector<int> nums = {3,6,7,11};
-    int h = 8;
-    cout << working(nums , h);
+    return st;
 }
