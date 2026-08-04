a665c3518fe720b8a3c5ec129b1b423a20f2a9a8
shaurya Negi <creatorsb2358@gmail.com>
2026-08-04 22:33:13 +0530

    bubble sort

--- questions.cpp
+++ questions.cpp
@@ -1,40 +1,36 @@
-///find the first and last occurence:
+///Bubble sort:
 
 #include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
-int main(){
-    vector<int> nums = {5,7,7,8,8,10};
-    int target = 8;
+int bubblesort(vector<int>& arr ){
+    int size = arr.size();
+    for(int i = 0 ; i< size ;i++){
+        bool istrue = false;
+        for(int j = 0 ;j < size-i-1;j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                istrue = true;
+            }
+        }
+        if(!istrue){
+            return 0 ;
+        }
+    }
+}
+
+int printarray(vector<int>& nums){
     int size = nums.size();
-    vector<int> arr;
-    int st = 0 , end = size -1, ans = -1;
-    while(st <= end){
-        int first = st + (end-st)/2;
-        if(nums[first] == target){
-            ans = first;
-            end = first -1;
-        }else if(nums[first] > target){
-            end = first -1;
-        }else if(nums[first]< target){
-            st = first +1;
-        }
+    for(int i = 0 ;i< size ;i++){
+        cout << nums[i] << " ";
     }
-    arr.push_back(ans);
-    int st1 = 0, end1 = size -1 , ans1 =-1;
-    while(st1 <= end1){
-        int last = st1 + (end1-st1)/2;
-        if(nums[last] == target){
-            ans1 = last;
-            st = last +1;
-        }else if(nums[last] > target){
-            end = last -1;
-        }else if(nums[last] < target){
-            st = last +1;
-        }
-    }
-    arr.push_back(ans1);
-    return arr;
+}
+
+int main(){
+    vector<int>nums = {1,3,2,5,4};
+    bubblesort(nums);
+    printarray(nums);
+    return 0;
 }
