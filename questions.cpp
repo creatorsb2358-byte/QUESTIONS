230825ff8baa29d7b158e13a04381a2047f5b51c
shaurya Negi <creatorsb2358@gmail.com>
2026-08-06 23:47:43 +0530

    revise

--- questions.cpp
+++ questions.cpp
@@ -1,30 +1,48 @@
-/// merge 2 sorted arrays:
+///revise capacirty to store d packages:
 
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
+class Solution {
+public:
+    bool isvalid(vector<int>& arr, int days , int limit){
+        int size = arr.size();
+        int weight = 0;
+        int day = 1 ;
+        for(int i = 0 ; i < size ; i++ ){
+            if(arr[i] > limit){
 
-int main(){
-    vector<int> nums1 = {1,2,3,0,0,0};
-    vector<int> nums2 = {4,5,6};
-    int size = nums1.size();
-    int m = 3 , n = 3;
-    int i = m-1 , j = n-1 , idx = size -1;
-    while( i >=0 && j >= 0){
-        if(nums1[i] >= nums2[j]){
-            nums1[idx] = nums1[i];  
-            idx-- , i--;
+                return false;
+            }
+            if(weight + arr[i] <= limit){
+                weight += arr[i];
+            }else{
+                day++;
+                weight = arr[i];
+            }
+        }
+        if(day <= days){
+            return true;
         }else{
-            nums1[idx] = nums2[j];
-            idx--, j--;
+            return false;
         }
     }
-    while(j >= 0){
-        nums1[idx] = nums2[j];
-        idx-- , j--;
+    int shipWithinDays(vector<int>& weights, int days) {
+        int sum = 0;
+        int size = weights.size();
+        for(int i = 0 ;i < size ; i++){
+            sum += weights[i];
+        }
+        int st = *max_element(weights.begin(), weights.end()) , end =sum ;
+        int ans = -1;
+        while(st <= end){
+            int mid = st + (end-st)/2;
+            if(isvalid(weights , days , mid)){
+                ans = mid;
+                end = mid-1;
+            }else{
+                st = mid+1;
+            }
+        }
+        return ans ;
+
+        
     }
-    for(int i : nums1){
-        cout << i;
-    }
-}
+};
