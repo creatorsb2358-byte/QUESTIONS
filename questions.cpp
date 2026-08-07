3fffacccde905eacdc6bef6c985b3229347ba694
shaurya Negi <creatorsb2358@gmail.com>
2026-08-07 23:54:28 +0530

    next permutation

--- questions.cpp
+++ questions.cpp
@@ -1,48 +1,33 @@
-///revise capacirty to store d packages:
+///next permutation:
 
-class Solution {
-public:
-    bool isvalid(vector<int>& arr, int days , int limit){
-        int size = arr.size();
-        int weight = 0;
-        int day = 1 ;
-        for(int i = 0 ; i < size ; i++ ){
-            if(arr[i] > limit){
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
-                return false;
-            }
-            if(weight + arr[i] <= limit){
-                weight += arr[i];
-            }else{
-                day++;
-                weight = arr[i];
-            }
-        }
-        if(day <= days){
-            return true;
-        }else{
-            return false;
+int main(){
+    vector<int> nums = {1,2,5,4,3};
+    int size = nums.size();
+    int pi = -1;
+    for(int i = size-2; i >= 0 ; i--){
+        if(nums[i] < nums[i+1]){
+            pi = i;
+            break;
         }
     }
-    int shipWithinDays(vector<int>& weights, int days) {
-        int sum = 0;
-        int size = weights.size();
-        for(int i = 0 ;i < size ; i++){
-            sum += weights[i];
-        }
-        int st = *max_element(weights.begin(), weights.end()) , end =sum ;
-        int ans = -1;
-        while(st <= end){
-            int mid = st + (end-st)/2;
-            if(isvalid(weights , days , mid)){
-                ans = mid;
-                end = mid-1;
-            }else{
-                st = mid+1;
-            }
-        }
-        return ans ;
-
-        
+    if(pi == -1){
+        reverse(nums.begin(), nums.end());
+        return ;
     }
-};
+    for(int i = size -1 ; i> pi ; i--){
+        if(nums[i] > nums[pi]){
+            swap(nums[i], nums[pi]);
+            break;
+        }
+    }
+    int i = pi+1 , j = size-1;
+    while(i <= j){
+        swap(nums[i],nums[j]);
+        i++,j--;
+    }
+}
