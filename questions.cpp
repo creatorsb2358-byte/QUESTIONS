b180227595e11b6b5540b3349f6f7620218cac39
shaurya Negi <creatorsb2358@gmail.com>
2026-08-08 23:27:20 +0530

    first bad version

--- questions.cpp
+++ questions.cpp
@@ -1,33 +1,27 @@
-///next permutation:
+///first bad version:
 
 #include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
+bool isbadversion(int n){
+    if(n >= 5){
+        return true;
+    }
+}
+
 int main(){
-    vector<int> nums = {1,2,5,4,3};
-    int size = nums.size();
-    int pi = -1;
-    for(int i = size-2; i >= 0 ; i--){
-        if(nums[i] < nums[i+1]){
-            pi = i;
-            break;
+    int n = 8;
+    int st = 1 , end = n , ans = 0;
+    while(st <= end){
+        int mid = st + (end-st)/2;
+        if(isbadversion(mid)){
+            ans = mid;
+            end = mid-1;
+        }else{
+            st = mid+1;
         }
     }
-    if(pi == -1){
-        reverse(nums.begin(), nums.end());
-        return ;
-    }
-    for(int i = size -1 ; i> pi ; i--){
-        if(nums[i] > nums[pi]){
-            swap(nums[i], nums[pi]);
-            break;
-        }
-    }
-    int i = pi+1 , j = size-1;
-    while(i <= j){
-        swap(nums[i],nums[j]);
-        i++,j--;
-    }
+    cout << ans;
 }
