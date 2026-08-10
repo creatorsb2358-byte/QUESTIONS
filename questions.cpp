4a1aef2ef60a9d76a4739e64d288fa194e1dd453
shaurya Negi <creatorsb2358@gmail.com>
2026-08-10 22:47:37 +0530

    valid palindrome

--- questions.cpp
+++ questions.cpp
@@ -1,17 +1,31 @@
-///Reverse of string:
+///valid palindrome:
 
 #include <iostream>
 #include <vector>
 #include <string>
 using namespace std;
 
-int main(){
-    vector<char> s = {'h','e','l','l','o'};
-    int st = 0 , end = s.size()-1;
-    while(st < end){
-        swap(s[st++],s[end--]);
+bool isalphanumeric(char ch){
+    if((ch >='0' && ch<= '9') ||
+        (tolower(ch) >= 'a' && tolower(ch) <= 'z')){
+            return true;
+        }
+    return false;
+}
+bool isPalindrome(string s) {
+    int st = 0 , end = s.length()-1;
+    while(st <= end){
+        if(!isalphanumeric(s[st])){
+            st++; continue;
+        }
+        if(!isalphanumeric(s[end])){
+            end--; continue;
+        }
+        if(tolower(s[st]) != tolower(s[end])){
+            return false;
+        }
+        st++ , end--;
     }
-    for(char s : s){
-        cout << s;
-    }
+    return true;
+        
 }
