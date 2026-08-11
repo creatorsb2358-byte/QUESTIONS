67925abfa438d6a3aef686a93518017261c71f29
shaurya Negi <creatorsb2358@gmail.com>
2026-08-11 23:12:38 +0530

    remove all occurences in a substring

--- questions.cpp
+++ questions.cpp
@@ -5,27 +5,12 @@
 #include <string>
 using namespace std;
 
-bool isalphanumeric(char ch){
-    if((ch >='0' && ch<= '9') ||
-        (tolower(ch) >= 'a' && tolower(ch) <= 'z')){
-            return true;
-        }
-    return false;
-}
-bool isPalindrome(string s) {
-    int st = 0 , end = s.length()-1;
-    while(st <= end){
-        if(!isalphanumeric(s[st])){
-            st++; continue;
-        }
-        if(!isalphanumeric(s[end])){
-            end--; continue;
-        }
-        if(tolower(s[st]) != tolower(s[end])){
-            return false;
-        }
-        st++ , end--;
+int main(){
+    string s = "abbabcbabcbb";
+    string part = "abc";
+    while(s.find(part)<s.length() && s.length()>0)
+    {
+        s.erase(s.find(part),part.length());
     }
-    return true;
-        
+    cout << s;
 }
