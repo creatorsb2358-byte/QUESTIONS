43ae2742e293c3eef112541948cf919f9aaf9e16
shaurya Negi <creatorsb2358@gmail.com>
2026-08-12 23:54:41 +0530

    sort colours

--- questions.cpp
+++ questions.cpp
@@ -1,16 +1,26 @@
-///valid palindrome:
+///sort colours:
 
 #include <iostream>
 #include <vector>
-#include <string>
+#include <algorithm>
 using namespace std;
 
 int main(){
-    string s = "abbabcbabcbb";
-    string part = "abc";
-    while(s.find(part)<s.length() && s.length()>0)
-    {
-        s.erase(s.find(part),part.length());
+    vector<int> nums = {1,2,0,2,1,1,2,0,0};
+    int size = nums.size();
+    int low = 0 , mid = 0 , end = size - 1;
+    while(mid <= end){
+        if(nums[mid] == 0){
+            swap(nums[mid],nums[low]);
+            low++ , mid++;
+        }else if(nums[mid] == 2){
+            swap(nums[mid],nums[end]);
+            end--;
+        }else{
+            mid++;
+        }
     }
-    cout << s;
+    for(int i : nums){
+        cout << i << " " ;
+    }
 }
