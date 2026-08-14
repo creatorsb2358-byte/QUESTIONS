e98f93ba0299593b5110f3466a3b27556af0dffb
shaurya Negi <creatorsb2358@gmail.com>
2026-08-14 22:59:00 +0530

    reverse words in a string

--- questions.cpp
+++ questions.cpp
@@ -1,37 +1,25 @@
-///permutation in string:
+///reverse words in a string:
 
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 using namespace std;
 
-
-bool isfreqsame(int freq1[] , int freq2[]){
-    for(int i = 0 ; i<26 ;i++){
-        if(freq1[i] != freq2[i]){
-            return false;
+int main(){
+    string s = "the sky is blue";
+    reverse(s.begin(),s.end());
+    string ans = "";
+    for(int i = 0 ; i< s.length(); i++){
+        string word = "";
+        while(i<s.length() && s[i] != ' '){
+            word += s[i];
+            i++;
+        }
+        reverse(word.begin(),word.end());
+        if(word.length()>0){
+            ans += " " + word;
         }
     }
-    return true;
-}
-bool checkInclusion(string s1, string s2) {
-    int freq1[26] = {0};
-    for(int i = 0 ; i<s1.length(); i++){
-        freq1[s1[i]-'a']++;
-    }
-        
-    int windowsize = s1.length();
-    for(int i = 0 ;i<s2.length();i++){
-        int windidx = 0 , idx = i;
-        int freq2[26]={0};
-        while(windidx < windowsize && idx < s2.length()){
-            freq2[s2[idx]-'a']++;
-            windidx++, idx++;
-        }
-        if(isfreqsame(freq1, freq2)){
-            return true ;
-        }
-    }
-    return false;
-        
+    return ans.substr(1);
 }
