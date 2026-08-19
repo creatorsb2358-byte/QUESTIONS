8d9db173b449595d747a24d0b105a88fed79f0ab
shaurya Negi <creatorsb2358@gmail.com>
2026-08-19 20:54:49 +0530

    Missing number

--- questions.cpp
+++ questions.cpp
@@ -1,4 +1,4 @@
-/// permutation in string revision:
+/// Missing number:
 
 #include <iostream>
 #include <vector>
@@ -6,33 +6,16 @@
 #include <algorithm>
 using namespace std;
 
-bool ispermutation(int freq1[], int freq2[]){
-    for(int i = 0 ; i < 26 ; i++){
-        if(freq1[i] == freq2[i]){
-            return true;
-        }
-    }
-    return false;
-}
-
 int main(){
-    string part = {'a','b'};
-    string s = {'e','i','d','b','a','o','o'};
-    int freq1[26];
-    for(int i = 0 ; i< 26 ;i++){
-        freq1[part[i] - 'a']++;
+    vector<int> nums = {0,3,1};
+    int sum = 0 , sum1 = 0 ;
+    for(int i = 0 ; i <= nums.size() ; i++){
+        sum += i;
     }
-    int windowsize = part.length();
-    for(int i = 0 ; i < s.length() ; i++){
-        int windidx = 0 , idx = i ;
-        int freq2[26];
-        while(windidx < windowsize && idx < s.length()){
-            freq2[s[idx]-'a']++;
-            windidx++ , idx++;
-        }
-        if(ispermutation(freq1 , freq2)){
-            return true;
-        }
+    for(int i = 0 ; i< nums.size() ; i++){
+        sum1 += nums[i];
     }
-    return false;
+    int ans = sum - sum1;
+    cout << ans; 
+    return 0 ;
 }
