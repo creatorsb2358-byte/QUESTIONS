102b8af93cab9c7ef7313c4460329f59d740534b
shaurya Negi <creatorsb2358@gmail.com>
2026-08-20 20:59:17 +0530

    max sum of a row in matrix

--- questions.cpp
+++ questions.cpp
@@ -1,4 +1,4 @@
-/// Missing number:
+/// Max sum of a row in matrix:
 
 #include <iostream>
 #include <vector>
@@ -7,15 +7,17 @@
 using namespace std;
 
 int main(){
-    vector<int> nums = {0,3,1};
-    int sum = 0 , sum1 = 0 ;
-    for(int i = 0 ; i <= nums.size() ; i++){
-        sum += i;
+    int matrix[4][3] = {{1,2,3},{4,5,6},{7,8,9},{10,11,12}};
+    int row = 4 , column = 3;
+    int ans = INT32_MIN;
+    
+    for(int i = 0 ; i< row ; i++){
+        int sum = 0 ;
+        for(int j = 0 ; j < column ; j++){
+            sum += matrix[i][j];
+            ans = max(ans, sum);
+        }
+       
     }
-    for(int i = 0 ; i< nums.size() ; i++){
-        sum1 += nums[i];
-    }
-    int ans = sum - sum1;
-    cout << ans; 
-    return 0 ;
+    cout << ans;
 }
