3001c61009ee1bb1d6dd36d3db766cf074ef6a0c
shaurya Negi <creatorsb2358@gmail.com>
2026-08-20 23:20:48 +0530

    search in a 2D matrix different appraoch

--- questions.cpp
+++ questions.cpp
@@ -1,4 +1,4 @@
-/// Max sum of a row in matrix:
+/// search in a 2D matrix:
 
 #include <iostream>
 #include <vector>
@@ -6,19 +6,42 @@
 #include <algorithm>
 using namespace std;
 
-int main(){
-    int matrix[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
-    int row = 4 , column = 3;
-    int ans = INT32_MIN;
-    int sum = 0 ;
-    for(int i = 0 ; i< row ; i++){
-       
-        for(int j = 0 ; j < column ; j++){
-            if(i==j){
-                sum+= matrix[i][j];
-            }
+bool istrue(vector<vector<int>> matrix , int target , int row){
+    int n = matrix[0].size();
+    int st = 0 , end = n-1;
+    while(st <= end){
+        int mid = st + (end-st)/2;
+        if(target == matrix[row][mid]){
+            return true;
+        }else if(target > matrix[row][mid]){
+            st = mid+1;
+        }else{
+            end = mid-1;
         }
-       
     }
-    cout << sum;
+    return false;
+}
+
+bool searchMatrix(vector<vector<int> > &matrix, int target){
+    int m = matrix.size();
+    int n = matrix[0].size();
+    int st = 0 , end = m-1;
+    while(st <= end){
+        int mid = st + (end-st)/2;
+        if(target >= matrix[mid][0] && target <= matrix[mid][n-1]){
+            istrue(matrix , target , mid );
+            return true;
+        }else if(target >= matrix[mid][n-1]){
+            st = mid +1 ;
+        }else {
+            end = mid-1;
+        }
+    }
+    return false;
+}
+
+int main(){
+    vector<vector<int>> matrix = {{1,2,3},{4,5,6},{7,8,9}};
+    int target = 8;
+    cout << searchMatrix(matrix , target);
 }
