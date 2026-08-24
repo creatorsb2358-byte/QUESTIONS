ff5261527a84d9bece582ec6772e354632ff1d1e
shaurya Negi <creatorsb2358@gmail.com>
2026-08-24 21:50:08 +0530

    3Sum

--- questions.cpp
+++ questions.cpp
@@ -1,22 +1,34 @@
-/// Running sum in 1d array:
+/// 3Sum:
 
 #include <iostream>
 #include <vector>
 #include <string>
-#include <unordered_map>
+#include <algorithm>
 using namespace std;
+
 int main(){
-    vector<int> nums = {1,2,3,4,5};
-    vector<int> ans;
-    int sum = 0;
+    vector<int> nums = {-1,0,1,2,-1,-4};
+    vector<vector<int>> ans;
     for(int i = 0 ; i<nums.size(); i++){
-        sum += nums[i];
-        nums[i]= sum;
-        ans.push_back(nums[i]);
-        
-    }
-    for(int i : ans){
-        cout << i;
+        if(i > 0 && nums[i] == nums[i-1]){
+            continue;
+        }
+        int st = i+1 , end =nums.size()-1;
+        while(st < end){
+            int sum = nums[i] + nums[st] + nums[end];
+            if(sum > 0){
+                end--;
+            }else if(sum <0){
+                st++;
+            }else{
+                ans.push_back({nums[i],nums[st],nums[end]});
+                st++,end--;
+            }
+            while(st > 0 && nums[st] == nums[st-1]){
+                st++;
+            }
+        }
+        return ans;
     }
 
 }
