d0f6fe7dc3642ff2356ea5b48e19931a6d5443fb
shaurya Negi <creatorsb2358@gmail.com>
2026-09-01 21:41:16 +0530

    palindrome partitions

--- questions.cpp
+++ questions.cpp
@@ -6,31 +6,34 @@
 #include <algorithm>
 using namespace std;
 
-void anss(vector<int>& candidates , int currsum , int curridx ,int target, vector<vector<int>>& ans , vector<int>& small){
-    if(currsum > target) return ;
-    if(currsum == target){
-        ans.push_back(small);
-        return ;
-    }
-    for(int i = curridx; i<candidates.size(); i++){
-        currsum += candidates[i];
-        small.push_back(candidates[i]);
-        anss(candidates,currsum , i , target , ans , small);
-        small.pop_back();
-    }
-
+bool ispalin(string part){
+    string part2 = part;
+    reverse(part2.begin(),part2.end());
+    return part2 == part;
 }
 
-vector<vector<int>> combsum(vector<int>& candidates, int target){
-    int currsum = 0 , curridx  = 0 ;
-    vector<int> small ;
-    vector<vector<int>> ans;
-    anss(candidates , currsum,curridx,target,ans, small);
+void palpart(string s ,vector<string>& partitions, vector<vector<string>>& ans){
+    if(s.size() == 0){
+        ans.push_back(partitions);
+        return;
+    }
+    for(int i = 0 ; i< s.size(); i++){
+        string part = s.substr(0,i+1);
+        if(ispalin(part)){
+            partitions.push_back(part);
+            palpart(s.substr(i+1),partitions,ans);
+            partitions.pop_back();
+        }
+    }
+}
+vector<vector<string>> partition(string s) {
+    vector<string> partitions;
+    vector<vector<string>> ans;
+    palpart(s, partitions, ans);
     return ans;
 }
 
 int main(){
-    vector<int> candidates = {2,3,5};
-    int target = 8;
-    combsum(candidates,target);
+    string s = "aab";
+    partition(s);
 }
