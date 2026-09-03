80641a2ce91c686d00e71421830247f67054dd96
shaurya Negi <creatorsb2358@gmail.com>
2026-09-03 22:08:39 +0530

    combination sum 2

--- questions.cpp
+++ questions.cpp
@@ -1,4 +1,4 @@
-/// combinations:
+/// combination sum 2:
 
 #include <iostream>
 #include <vector>
@@ -6,28 +6,38 @@
 #include <algorithm>
 using namespace std;
 
-void solve(int start, int n , int k , vector<int>& path, vector<vector<int>>& ans){
-    if(path.size() == k){
-        ans.push_back(path);
+void solve(vector<int>& candidates, int idx , int sum ,int target ,vector<int>& small , vector<vector<int>>& ans){
+    if(sum > target) return;
+
+    if(sum == target){
+        ans.push_back(small);
         return;
     }
-    for(int i = start ; i<=n ; i++){
-        path.push_back(i);
-        solve(i+1,n,k,path,ans);
-        path.pop_back();
+
+    for(int i = idx ; i<candidates.size(); i++){
+        small.push_back(candidates[i]);
+        sum += candidates[i];
+        solve(candidates,i+1,sum, target,small,ans);
+        small.pop_back();
+        sum-= candidates[i];
+        int idx = i+1;
+        while(idx < candidates.size() && candidates[idx] == candidates[idx-1]){
+            i++,idx++;
+        }
     }
 }
-
-vector<vector<int>> combine(int n, int k) {
-    vector<int> path;
+vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+    sort(candidates.begin(),candidates.end());
+    vector<int>small ;
     vector<vector<int>> ans;
-    int start = 1;
-    solve(start, n,k,path,ans);
+    int sum = 0;
+    int idx = 0;
+    solve(candidates,idx,sum , target , small , ans);
     return ans;
 }
 
 int main(){
-    int n= 4;
-    int k = 2;
-    combine(n,k);
+    vector<int> candidates={2,1,2,5,2};
+    int target = 5;
+    combinationSum2(candidates,target);
 }
